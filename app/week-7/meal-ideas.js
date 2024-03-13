bbbb1872e9ef6553c79f6bfdbfc626b231ec5750
mitul72 <mituldhawan2@gmail.com>
2024-03-12 20:40:53 -0600

    add challenge 2

--- app/week-7/meal-ideas.js
+++ app/week-7/meal-ideas.js
@@ -1,7 +1,16 @@
 "use client";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader } from "@/components/ui/card";
+import {
+  Drawer,
+  DrawerTrigger,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
 
 async function fetchMealIdeas(ingredient) {
   try {
@@ -14,12 +23,34 @@ async function fetchMealIdeas(ingredient) {
     console.error(error);
   }
 }
+async function fetchMealDetails(id) {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default function MealIdeas({ ingredient, setIngredient }) {
+  const [ingredients, setIngredients] = useState([]);
   const [mealIdeas, setMealIdeas] = useState(null);
   const loadMealIdeas = async () => {
     const data = await fetchMealIdeas(ingredient);
     setMealIdeas(data.meals);
+  };
+  const loadIngredients = async (id) => {
+    const data = await fetchMealDetails(id);
+    setIngredients(
+      Object.keys(data.meals[0])
+        .filter((key) => key.startsWith("strIngredient"))
+        .map((key) => data.meals[0][key])
+        .filter((ingredient) => ingredient)
+    );
   };
   useEffect(() => {
     loadMealIdeas();
@@ -34,9 +65,31 @@ export default function MealIdeas({ ingredient, setIngredient }) {
         {mealIdeas ? (
           mealIdeas.map((meal) => {
             return (
-              <Card key={meal.idMeal}>
-                <CardHeader>{meal.strMeal}</CardHeader>
-              </Card>
+              <Drawer>
+                <DrawerTrigger>
+                  <Card
+                    key={meal.idMeal}
+                    onClick={() => {
+                      loadIngredients(meal.idMeal);
+                    }}
+                  >
+                    <CardHeader>{meal.strMeal}</CardHeader>
+                  </Card>
+                  <DrawerContent>
+                    <DrawerHeader>
+                      <DrawerTitle>Ingredients for {meal.strMeal}</DrawerTitle>
+                      {ingredients.map((ingredient) => (
+                        <DrawerDescription key={ingredient}>
+                          {ingredient}
+                        </DrawerDescription>
+                      ))}
+                    </DrawerHeader>
+                    <DrawerClose>
+                      <Button variant="outline">Cancel</Button>
+                    </DrawerClose>
+                  </DrawerContent>
+                </DrawerTrigger>
+              </Drawer>
             );
           })
         ) : (
